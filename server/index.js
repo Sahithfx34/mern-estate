aa823e695d03341a68d2100c104ebc70aa5af158
Sahith <sahithreddyfx34@gmail.com>
2023-10-10 15:47:20 +0530

    created user model
    Signed-off-by: Sahith <sahithreddyfx34@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import "dotenv/config"
+import mongoose from "mongoose";
+
 const app = express();
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    app.listen(3000,()=>{
+        console.log("Running on Port 3000")
+    })
+}).catch((err)=>{
+        console.log(err);
+});
 
 
-app.listen(process.env.PORT,()=>{
-    console.log("Running on Port 3000");
-})
