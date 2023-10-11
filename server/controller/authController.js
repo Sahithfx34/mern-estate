7ef1d999c9a1580c290c8ab9a19df07e1557440a
Sahith <sahithreddyfx34@gmail.com>
2023-10-11 10:23:32 +0530

    create a middleware to handle error
    Signed-off-by: Sahith <sahithreddyfx34@gmail.com>

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,7 +1,7 @@
 import User from "../Models/userModel.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
     const {username,email,password}= req.body;
     const hashed = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password: hashed});
@@ -9,7 +9,7 @@ export const signup = async(req,res)=>{
         await newUser.save();
         res.status(201).json({msg:"user created succuessfully"});
     }catch(err){
-        res.status(500).json(err.message);
+        next(err);
     }
 
 } 
--- server/index.js
+++ server/index.js
@@ -19,3 +19,13 @@ mongoose.connect(process.env.MONGO)
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const msg = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        msg,
+    });
+});
